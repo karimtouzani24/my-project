d11398c60afc8c214ae93f0c09af4f720ae23588
karimtouzani24 <karimtouzani24@outlook.com>
2022-12-08 09:19:21 +0100

    update

--- my_project/my_project_stack.py
+++ my_project/my_project_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     
+    aws_ec2 as ec2,
     Stack,
-    # aws_sqs as sqs,
+    
 )
 from constructs import Construct
 
@@ -10,9 +11,27 @@ class MyProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc1 = ec2.Vpc(self, id="Vpc1",
+        max_azs=2,
+        ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
+        subnet_configuration=[
+            ec2.SubnetConfiguration(
+                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                cidr_mask=26,
+                name="Private-subnet-test"
+            )
+        ]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyProjectQueue",
-        # )
+        vpc2 = ec2.Vpc(self, id="Vpc2",
+        max_azs=2,
+        ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
+        subnet_configuration=[
+            ec2.SubnetConfiguration(
+                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                cidr_mask=26,
+                name="Private-subnet-test"
+            )
+        ]
+        )
+                  
