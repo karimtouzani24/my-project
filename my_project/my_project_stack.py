99f21118dc04d65f4c5b44d3424c73b8cd26bdd8
karimtouzani24 <karimtouzani24@outlook.com>
2022-12-09 17:00:32 +0100

    update

--- my_project/my_project_stack.py
+++ my_project/my_project_stack.py
@@ -1,6 +1,7 @@
 import aws_cdk as cdk
 from aws_cdk import (
     aws_ec2 as ec2,
+    aws_s3 as s3,
     Stack,    
 )
 from constructs import Construct
@@ -10,41 +11,47 @@ class MyProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        vpc1 = ec2.Vpc(self, "Vpc_W",
-        max_azs=2,
-        ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
-        subnet_configuration=[
-            ec2.SubnetConfiguration(
-                name="Public_subnet_W1",
-                subnet_type=ec2.SubnetType.PUBLIC,
-                cidr_mask = 26
-            ),
-            ec2.SubnetConfiguration(
-                name="Public_subnet_W2",
-                subnet_type=ec2.SubnetType.PUBLIC,
-                cidr_mask= 26
-            )
-        ])
+        # creating vpc, webserver.
+        vpc1 = ec2.Vpc(
+            self, 
+            "Vpc_W",
+            max_azs=2,
+            ip_addresses=ec2.IpAddresses.cidr("10.10.10.0/24"),
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Public_subnet_W1",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask = 26),
+                ec2.SubnetConfiguration(
+                    name="Public_subnet_W2",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask= 26)]
+        )
 
-        vpc2 = ec2.Vpc(self, "Vpc_M",
-        max_azs=2,
-        ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
-        subnet_configuration=[
-            ec2.SubnetConfiguration(
-                name="Public_subnet_M1",
-                subnet_type=ec2.SubnetType.PUBLIC,
-                cidr_mask=26,
-            ),
-            ec2.SubnetConfiguration(
-                name="Public_subnet_M2",
-                subnet_type=ec2.SubnetType.PUBLIC,
-                cidr_mask=26,
-            )
-        ])
+        # creating vpc, managment server.
+        vpc2 = ec2.Vpc(
+            self, 
+            "Vpc_M",
+            max_azs=2,
+            ip_addresses=ec2.IpAddresses.cidr("10.20.20.0/24"),
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Public_subnet_M1",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=26,),
+                ec2.SubnetConfiguration(
+                    name="Public_subnet_M2",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=26,)]
+        )
 
-        cfn_cPCPeering_connection = ec2.CfnVPCPeeringConnection(self, "My_vpcPeeringConnection",
-        peer_vpc_id="Vpc_M",
-        vpc_id="Vpc_W")
+        # creating a vpc peering connection.
+        cfn_cPCPeering_connection = ec2.CfnVPCPeeringConnection(
+            self, 
+            "My_vpcPeeringConnection",
+            peer_vpc_id= vpc1.vpc_id,
+            vpc_id= vpc2.vpc_id
+        )
 
         # creating linux AMI for the WEB server.
         amzn_linux = ec2.MachineImage.latest_amazon_linux(
@@ -56,21 +63,88 @@ class MyProjectStack(Stack):
        #creating Windows Ami for the MGMT server.
         amzn_windows = ec2.MachineImage.latest_windows(
             version=ec2.WindowsVersion.WINDOWS_SERVER_2022_ENGLISH_FULL_BASE
-       )
+        )
 
        #creating instance, WEB server, linux.
-        web_server = ec2.Instance(self, "web_instance",
-        vpc=vpc1,
-        machine_image=amzn_linux,
-        instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO))
+        web_server = ec2.Instance(
+            self, 
+            "web_instance",
+            vpc= vpc1,
+            machine_image= amzn_linux,
+            instance_type= ec2.InstanceType.of(
+                ec2.InstanceClass.T2,
+                ec2.InstanceSize.MICRO)
+        )
 
         #creating instance, MMGMT serever, windows.
-        mngmt_server = ec2.Instance(self, "mngmt_instance",
-        vpc=vpc2,
-        machine_image=amzn_windows,
-        instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2,
-        ec2.InstanceSize.MICRO))
-       
+        mngmt_server = ec2.Instance(
+            self, 
+            "mngmt_instance",
+            vpc= vpc2,
+            machine_image= amzn_windows,
+            instance_type= ec2.InstanceType.of(
+                ec2.InstanceClass.T2,
+                ec2.InstanceSize.MICRO)
+        )
+
+        # creating SG webserver
+        webserver_SG = ec2.SecurityGroup(
+            self, 
+            "web_SG",
+            vpc=vpc1,
+            allow_all_outbound= True,
+            description= "webserver security group"
+        )
+
+        webserver_SG.add_ingress_rule(
+            peer= ec2.Peer.any_ipv4(),
+            connection= ec2.Port.tcp(80),
+            description= "allow http"
+        )
+
+        webserver_SG.add_ingress_rule(
+            peer= ec2.Peer.any_ipv4(),
+            connection= ec2.Port.tcp(443),
+            description= "allow https"
+        )
+
+        # creating SG MNGMT server.
+        mngmt_SG = ec2.SecurityGroup(
+            self, 
+            "mngmt_SG",
+            vpc= vpc2,
+            allow_all_outbound= True,
+            description= "mngmt security group"
+        )
+
+        mngmt_SG.add_ingress_rule(
+            peer= ec2.Peer.any_ipv4(),
+            connection= ec2.Port.tcp(80),
+            description= "allow http"
+        )
+        mngmt_SG.add_ingress_rule(
+            peer= ec2.Peer.any_ipv4(),
+            connection= ec2.Port.tcp(443),
+            description= "allow https"
+        )
+        mngmt_SG.add_ingress_rule(
+            peer= ec2.Peer.ipv4("192.168.178.1/24"), #temporarely ip address
+            connection= ec2.Port.tcp(22),
+            description= "allow ssh, admin ip"
+        )
+        mngmt_SG.add_ingress_rule(
+            peer= ec2.Peer.ipv4("192.168.178.1/24"), #temporarely ip address
+            connection= ec2.Port.tcp(3389),
+            description= "allow rdp, admin ip"
+        )
         
+        # creating a bucket
+        bucket = s3.Bucket(
+            self, 
+            "project_backup_bucket",
+            encryption= s3.BucketEncryption.KMS,
+            versioned= True,
+            removal_policy= cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects= True
+        )
                   
